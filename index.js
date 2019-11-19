b597130c5b2b50c739a626daa6c5630883063120
minhajspace <minhaj.space@gmail.com>
2019-11-19 20:09:28 +0530

    reduce method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const companies = [
-  { name: "amazon", category: "Online", start: 1990, end: 2019 },
+  { name: "amazon", category: "Online", start: 1990, end: 2036 },
   { name: "flipkart", category: "online", start: 2000, end: 2050 },
   { name: "mahindra", category: "motor", start: 1980, end: 2070 },
   { name: "hdfc", category: "bank", start: 1994, end: 2049 },
@@ -21,6 +21,7 @@ const age = [
   32,
   41,
   52,
+  5,
   15,
   65,
   92,
@@ -42,7 +43,6 @@ const age = [
   65,
   75,
   85,
-  65,
   25,
   36,
   41,
@@ -142,9 +142,46 @@ let NewIndex = [];
 // const squreroot = age.map(age => Math.sqrt(age)).map(age => age * 2);
 // console.log(squreroot);
 
-number = [2, 4, 5, 6, 8, 9];
-index = [];
+// const frined = [
+//   { name: "minhaj", age: 24, city: "lucknow" },
+//   { name: "urooj", age: 20, city: "lucknow" },
+//   { name: "vishal", age: 21, city: "lucknow" },
+//   { name: "shad", age: 25, city: "lucknow" }
+// ];
 
-num = number.map((number, index) => `index is ${index} value ${number * 2}`);
-console.log(num);
-sort();
+// index = [];
+
+// num = number.map((number, index) => `index is ${index} value ${number * 2}`);
+// console.log(num);                                      sort ()
+
+// let ageWise = frined.sort((old, young) => (old.age < young.age ? 1 : -1));
+
+// console.log(ageWise);
+
+// sortedCompany = companies.sort((c1, c2) => (c1 > c2 ? 1 : -1));
+// console.log(sortedCompany);
+
+// sortedAge = age.sort((a, b) => (a > b ? 1 : -1));
+// console.log(sortedAge);
+
+// const arr = [2, 5, 8, 1, 4].sort((a, b) => (a > b ? 1 : -1));
+// console.log(arr);
+// using for loop
+
+// ageSum = 0;
+
+// for (i = 0; i < age.length; i++) {
+//   ageSum += age[i];
+// }
+// console.log(ageSum);
+
+//                                     now using Reduce method
+
+const sumAge = age.reduce((total, age) => total + age, 0);
+console.log(sumAge);
+
+// companySum = companies.reduce(function(total, company) {
+//   return (total = company.start - company.end);
+// }, 0);
+
+// console.log(companySum);
