e5fc2db9d81f66a1ed8e9d127e8145c8e85833b3
minhajspace <minhaj.space@gmail.com>
2019-11-19 15:47:09 +0530

    puch data in array using for loop

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const companies = [
   { name: "amazon", category: "Online", start: 1990, end: 2019 },
   { name: "flipkart", category: "online", start: 2000, end: 2050 },
   { name: "mahindra", category: "motor", start: 1980, end: 2070 },
-  { name: "hdfc", category: "bank", stat: 1994, end: 2049 },
-  { name: "mircosoft", category: "technology", stat: 1970, end: 2050 },
+  { name: "hdfc", category: "bank", start: 1994, end: 2049 },
+  { name: "mircosoft", category: "technology", start: 1970, end: 2050 },
   { name: "intel", category: "software", start: 1990, end: 2040 },
   { name: "flipkart", category: "online", start: 1920, end: 2060 },
   { name: "tata", category: "motor", start: 1940, end: 2092 },
-  { name: "axis", category: "bank", stat: 1994, end: 2049 },
-  { name: "infosis", category: "technology", stat: 1970, end: 2013 }
+  { name: "axis", category: "bank", start: 1994, end: 2049 },
+  { name: "infosis", category: "technology", start: 1970, end: 2013 }
 ];
-
 // console.log(companies);
 
 const age = [
@@ -60,6 +59,9 @@ const age = [
 //   console.log(age);
 // });
 
-// companies.forEach(function(companies) {
-//   console.log(companies.name);
-// });
+for (i = 0; i < age.length; i++) {
+  if (age[i] <= 18) {
+    canDrink.push(age[i]);
+  }
+}
+console.log(canDrink);
